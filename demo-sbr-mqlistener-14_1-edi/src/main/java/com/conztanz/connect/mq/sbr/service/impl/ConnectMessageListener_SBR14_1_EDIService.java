1a4b74d0f1cee811303b30c17a0bb9f11e0cbaec
Emmanuel Gire <emmanuel.gire@conztanz.com>
2015-12-23 14:59:00 +0100

    XA transaction management enabled on JMS

--- demo-sbr-mqlistener-14_1-edi/src/main/java/com/conztanz/connect/mq/sbr/service/impl/ConnectMessageListener_SBR14_1_EDIService.java
+++ demo-sbr-mqlistener-14_1-edi/src/main/java/com/conztanz/connect/mq/sbr/service/impl/ConnectMessageListener_SBR14_1_EDIService.java
@@ -1,12 +1,12 @@
 package com.conztanz.connect.mq.sbr.service.impl;
 
-import javax.jms.Message;
-
+import org.apache.activemq.pool.XaPooledConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.conztanz.connect.mq.sbr.service.IConnectMessageListener_SBR;
 
-@Service(value="SBR_14_1_EDI_SERVICE")
+@Service(value = "SBR_14_1_EDI_SERVICE")
 public class ConnectMessageListener_SBR14_1_EDIService implements IConnectMessageListener_SBR {
 
 	@Override
@@ -15,6 +15,7 @@ public class ConnectMessageListener_SBR14_1_EDIService implements IConnectMessag
 
 		System.out.println("=> ConnectMessageListener_SBR14_1_EDIService.onMessage()");
 		System.out.println("message:" + message);
+
 		// start transaction
 		// clean message
 		// conversion en xml via smooks
@@ -29,23 +30,24 @@ public class ConnectMessageListener_SBR14_1_EDIService implements IConnectMessag
 		return message;
 	}
 
-//	@Override
-//	public void onMessage(Message message) {
-//		// TODO Auto-generated method stub
-//
-//		System.out.println("=> ConnectMessageListener_SBR14_1_EDIService.onMessage()");
-//		System.out.println("message:" + message);
-//		// start transaction
-//		// clean message
-//		// conversion en xml via smooks
-//		// conversion java via jibx (objet de type BINDING dans conztanz one -
-//		// AbstractTravelMediator)
-//		// => extraction TRAVEL à partir de BINDING
-//		// conversion des TRAVEL JSON via jackson
-//		// checks + insertion lifecycle
-//		// commit
-//		// appel (travel) AS ?
-//
-//	}
+	// @Override
+	// public void onMessage(Message message) {
+	// // TODO Auto-generated method stub
+	//
+	// System.out.println("=>
+	// ConnectMessageListener_SBR14_1_EDIService.onMessage()");
+	// System.out.println("message:" + message);
+	// // start transaction
+	// // clean message
+	// // conversion en xml via smooks
+	// // conversion java via jibx (objet de type BINDING dans conztanz one -
+	// // AbstractTravelMediator)
+	// // => extraction TRAVEL à partir de BINDING
+	// // conversion des TRAVEL JSON via jackson
+	// // checks + insertion lifecycle
+	// // commit
+	// // appel (travel) AS ?
+	//
+	// }
 
 }
